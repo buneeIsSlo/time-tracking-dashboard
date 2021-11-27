ccace8cdd080e37d5482d975ea6c1bdd4f0f2d74
bunee <buneeisslo@gmail.com>
2021-11-27 17:32:46 +0530

    Add: added env variables and refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,62 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    mode: "production",
+module.exports = env => {
+    let devType = env.production || false;
+    console.log(devType);
 
-    entry: "./src/js/app.js",
+    return {
+        mode: devType ? "production" : "development",
 
-    output: {
-      filename: "js/app.bundle.js",
-      path: path.resolve(__dirname, "docs"),
-    },
+        entry: "./src/js/app.js",
 
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, "./docs")
+        output: {
+            filename: "js/app.bundle.js",
+            path: path.resolve(__dirname, "docs"),
+            assetModuleFilename: "imgs/[name][ext]",
+            clean: true
         },
-        port: 5001
-    },
 
-    module: {
-        rules: [
-            {
-                test: /\.html/g,
-                use: ["html-loader"]
-            }
+        devServer: {
+            static: {
+                directory: path.resolve(__dirname, "./docs")
+            },
+            port: 5001
+        },
+
+        module: {
+            rules: [{
+                    test: /\.html/g,
+                    use: ["html-loader"]
+                },
+
+                {
+                    test: /\.(svg|ico|webp|png|jpg|jpeg|gif)$/,
+                    type: "asset/resource"
+                },
+
+                {
+                    test: /\.css/g,
+                    use: [
+                        devType ? MiniCssExtractPlugin.loader : "style-loader",
+                        {
+                            loader: "css-loader"
+                        }
+                    ]
+                }
+            ]
+        },
+
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: "index.html",
+                template: path.resolve(__dirname, "./src/index.html")
+            }),
+            new MiniCssExtractPlugin({
+                filename: "css/main.css"
+            })
         ]
-    },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: path.resolve(__dirname, "./src/index.html")
-        })
-    ]
-}
+    };
+};
