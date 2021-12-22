6afcfb4b20050fef738b9f8d129b1a3214413fdd
bunee <buneeisslo@gmail.com>
2021-12-22 14:31:40 +0530

    Feat: imported animation.js and switchData.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,27 @@
+/* CSS imports */
 import "../css/normalize.css";
 import "../css/variables.css";
+import "../css/loader.css";
 import "../css/app.css";
 
+/* JS imports */
+import SwitchData from "../js/switchData.js";
+import Animation from "../js/animation.js";
 
-console.log("bunee");
+const animation = new Animation;
+const switchData = new SwitchData;
+const statsCard = Array.from(document.querySelectorAll(".card__stats"));
+
+/* Stats card background effect on hover */
+statsCard.forEach(card => {
+    card.addEventListener("mousemove", event => {
+        let hori = event.pageX - card.offsetLeft;
+        let vert = event.pageY - card.offsetTop;
+
+        card.style.setProperty("--top", hori + "px");
+        card.style.setProperty("--left", vert + "px");
+    });
+});
+
+/* Attribution :) */
+console.log("(\\ /)\n(0_0) Coded this.");
